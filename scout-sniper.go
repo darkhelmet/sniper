9ca5c7276a00d344e5fb3f1ed99c2651aec7cf0a
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2011-02-10 22:27:30 -0700

    Basic process monitoring, in that it restarts the process when it dies.
    It doesn't daemonize since Go apparently doesn't like doing that, since it's a pain with the thread system they have. Not a big deal, but then using something like daemonize chdirs by default to /. Something to think about...

--- scout-sniper.go
+++ scout-sniper.go
@@ -1,12 +1,43 @@
 package main
 
 import (
-    "fmt"
-    // "flag"
+    "exec"
+    "flag"
+    "os"
+    "time"
 )
 
-// runCommand = flag
+const Seconds = 1e9
+
+var interval = flag.Int64("interval", 2, "The interval to do the basic check")
+
+type QC chan bool
+
+func pump(msg bool, chans []QC) {
+    for _, c := range(chans) {
+        c <- msg
+        close(c)
+    }
+}
+
+func check(interval int64, quit chan bool, f func()) {
+    for {
+        time.Sleep(interval)
+        select {
+        case <- quit:
+            return
+        default:
+            f()
+        }
+    }
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+    flag.Parse()
+    cwd, _ := os.Getwd()
+    binary, _ := exec.LookPath(flag.Args()[0])
+    for {
+        cmd, _ := exec.Run(binary, flag.Args(), nil, cwd, exec.PassThrough, exec.PassThrough, exec.PassThrough)
+        cmd.Wait(os.WSTOPPED)
+    }
 }
